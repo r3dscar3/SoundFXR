652e559815418c6a175e8c2f5861abd996450ed5
Nolan Thompson <nolanthompson34@gmail.com>
2017-03-22 13:39:29 -0700

    some changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 $(document).ready(function () {
-    
-    $('.stop').on('click', function () {
-        window.location.reload(true);
-    });
-    
-    var sfxBaseUrl = "sfx/";
-    var songBaseUrl = "songs/";
+    var sfxBaseUrl = "./sfx/";
+    var songBaseUrl = "./songs/";
     var sfxAudio = ["thunder-loud.mp3", "telephone_ringing.mp3", "glass_shatter.mp3", "dial_tone.mp3", "Firework-1.wav", "Firework-2.mp3"];
-    var songAudio = ["Circus-music.mp3", "funeral-piano.mp3", "airplane.mp3", "rain-thunder.mp3", "mystery-jam.mp3", "park-noises.mp3", "Laurel-and-Hardy.mp3"];
+    var songAudio = ["Circus-music.mp3", "funeral-piano.wav", "airplane.mp3", "rain-thunder.mp3", "mystery-jam.mp3", "park-noises.mp3", "Laurel-and-Hardy.mp3"];
 
     $('.sfx .btn').on('click', function () {
         var sfx = $(this).attr('id').substring(1); //get the index of button
@@ -22,5 +17,9 @@ $(document).ready(function () {
         var songPlaying = new Audio(songBaseUrl + songAudio[song - 1]);
 
         songPlaying.play();
+    });
+
+    $('.stop').on('click', function () {
+      location.reload();
     });
 });
